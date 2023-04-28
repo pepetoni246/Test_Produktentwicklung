50ac4aed06c813bbc4e6c14fba0410882061cb49
Roman Mueller <roman.mueller.p@gmail.com>
2023-04-28 09:19:06 +0200

    sfa

---
+++ test_File.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+
+int main()
+{
+	// TODO
+	//ich bin ein kommentar
+
+
+	printf("hello world");
+	printf("hello");
+	printf("hallo");
+
+//comment
+//jhghj
+
+return 0;
+}
